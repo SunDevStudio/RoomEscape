de7f20b22c969c06c1c25a4c389539e8657e2337
SunDevStudio <sun__sun@windowslive.com>
2019-03-06 18:23:54 +0700

    OpenDoor-And-CreateScene

--- Source/RoomEscape/OpenDoor.cpp
+++ Source/RoomEscape/OpenDoor.cpp
@@ -1,35 +1,35 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
+#include "OpenDoor.h"
 #include "GameFramework/Actor.h"
 
-
-
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	
+	// ...
 }
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FString objName = GetOwner()->GetName();
-	FString objPos = GetOwner()->GetTransform().GetLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s HI %s"),*objName,*objPos);
+	AActor *owner = GetOwner();
+
+	FRotator newRotation = FRotator(0.f, -60.f, 0.f);
+
+	owner->SetActorRotation(newRotation);
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
